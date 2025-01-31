5dd6fea75aabcc55d4a251035699dc0bd1f4d7f1
Eldhose ER <eldhoseer16@gmail.comgit config --global user.email eldhoseer16@gmail.comgit config --global user.name Eldhose>
2025-01-31 13:11:41 +0530

    fix bug

--- index.js
+++ index.js
@@ -11,17 +11,13 @@ app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 
 mongoose.connect(process.env.MonGo_url).then(()=>{
-    
+
     console.log("iiiiiii DB CONNECTED");
 })
 
 
 
-app.use(cors({
-    origin:'https://menu-app-front-end.vercel.app/',
-    methods:['GET','POST','PUT','PATCH','DELETE'],//ALLOW SPECIFIC METHOD
-    // credentials:true,//allow cookies and authentications headers
-}))
+app.use(cors())
 
 app.use('/',require('./Routes/products'))
 
